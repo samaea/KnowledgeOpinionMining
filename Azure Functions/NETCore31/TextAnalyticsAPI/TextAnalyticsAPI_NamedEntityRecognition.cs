0ed3eafe3c8dfbe488e4f690faaf3c63d7196144
Sam EA <sam.elanis@microsoft.com>
2021-06-30 17:14:11 +0100

    Products API fix.

--- Azure Functions/NETCore31/TextAnalyticsAPI/TextAnalyticsAPI_NamedEntityRecognition.cs
+++ Azure Functions/NETCore31/TextAnalyticsAPI/TextAnalyticsAPI_NamedEntityRecognition.cs
@@ -13,7 +13,7 @@ using Newtonsoft.Json;
 using Azure.AI.TextAnalytics;
 using Azure;
 
-namespace TextAnalyticsAPINamedEntityRecognition
+namespace TextAnalyticsAPI_NamedEntityRecognition
 {
 
         public static class ProductCategory
@@ -74,12 +74,12 @@ namespace TextAnalyticsAPINamedEntityRecognition
 
         #region The Azure Function definition
 
-        [FunctionName("TextAnalyticsAPI-NamedEntityRecognition")]
+        [FunctionName("TextAnalyticsAPI_NamedEntityRecognition")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("TextAnalyticsAPI-NamedEntityRecognition function: C# HTTP trigger function processed a request.");
+            log.LogInformation("TextAnalyticsAPI_NamedEntityRecognition function: C# HTTP trigger function processed a request.");
 
             var response = new WebApiResponse
             {
@@ -181,26 +181,5 @@ namespace TextAnalyticsAPINamedEntityRecognition
 
         #endregion
     }
-    public static class TextAnalyticsAPI_NamedEntityRecognition
-    {
-        [FunctionName("TextAnalyticsAPI_NamedEntityRecognition")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
-    }
 }
